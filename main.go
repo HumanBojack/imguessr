cdcda14c7a8faccdefeba4f6299615c349737344
HumanBojack <spychalar59@gmail.com>
2022-11-17 19:58:31 +0100

    feat: create basic project structure

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getHello(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Hello World!")
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", getHello)
+
+	router.Run("localhost:8080")
+}
