f136530cb3b9c700af7dc7a5716723bf7c39a163
HumanBojack <spychalar59@gmail.com>
2023-09-03 23:11:06 +0200

    tests: GetAllUsers function

--- test/user_test.go
+++ test/user_test.go
@@ -52,3 +52,46 @@ func TestGetUser(t *testing.T) {
 		checkResponse(t, w, r.wantedCode, r.bodyIncl)
 	}
 }
+
+// Test the GetAllUsers function as an admin and non-admin
+func TestGetAllUsers(t *testing.T) {
+	// Get the auth token for the user
+	userToken := getAndParseToken(t, `{"name":"testLogin","password":"test1"}`)
+	t.Logf("User token : %v", userToken)
+	// Get the auth token for the admin
+	adminToken := getAndParseToken(t, `{"name":"testAdmin","password":"test1"}`)
+	t.Logf("Admin token : %v", adminToken)
+
+	// Set the different requests to test
+	requests := []struct {
+		wantedCode int
+		bodyIncl   string
+		token      string
+	}{
+		// Test without auth token
+		{http.StatusUnauthorized, "Authorization header is missing", ""},
+		// Test with invalid auth token
+		{http.StatusUnauthorized, "error", "invalid token"},
+		// Test as user
+		{http.StatusForbidden, `Unauthorized`, userToken},
+		// Test as admin
+		{http.StatusOK, `"testLogin"`, adminToken},
+	}
+
+	for _, r := range requests {
+		// Create a request to pass to our handler.
+		req, err := http.NewRequest("GET", "/v1/user/", nil)
+
+		if r.token != "" {
+			req.Header.Set("Authorization", "Bearer "+r.token)
+			t.Logf("Request with token : %v", r.token)
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w := mockRequest(req)
+		checkResponse(t, w, r.wantedCode, r.bodyIncl)
+	}
+}
