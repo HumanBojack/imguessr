7405138f2d15f5f273bda6186a0ea27ba7be3179
HumanBojack <spychalar59@gmail.com>
2023-09-04 00:09:41 +0200

    tests: UpdateUser as admin and non-admin

--- test/user_test.go
+++ test/user_test.go
@@ -145,3 +145,56 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
+// Test the UpdateUser function as an admin and non-admin
+// A non-admin can't update it's own admin status
+// A non-admin can't update other users
+func TestUpdateUser(t *testing.T) {
+	// Get the auth token for the user
+	userToken := getAndParseToken(t, `{"name":"testLogin","password":"test1"}`)
+	t.Logf("User token : %v", userToken)
+	// Get the auth token for the admin
+	adminToken := getAndParseToken(t, `{"name":"testAdmin","password":"test1"}`)
+	t.Logf("Admin token : %v", adminToken)
+
+	// Set the different requests to test
+	requests := []struct {
+		id         string
+		body       string
+		wantedCode int
+		bodyIncl   string
+		token      string
+	}{
+		// Test without auth token
+		{"39c71853-6206-4eef-9f5b-7a1a90830ccc", `{"isAdmin":true}`, http.StatusUnauthorized, "Authorization header is missing", ""},
+		// Test with invalid auth token
+		{"39c71853-6206-4eef-9f5b-7a1a90830ccc", `{"isAdmin":true}`, http.StatusUnauthorized, "error", "invalid token"},
+		// Test as user, promoting itself to admin
+		{"39c71853-6206-4eef-9f5b-7a1a90830ccc", `{"isAdmin":true}`, http.StatusOK, `"isAdmin":false`, userToken},
+		// Test as user, changing password
+		{"39c71853-6206-4eef-9f5b-7a1a90830ccc", `{"password":"newPwd"}`, http.StatusOK, `"password":"newPwd"`, userToken},
+		// Test as user, changing another user
+		{"ec4e2897-4ca4-4694-94d7-96db81ec223f", `{"password":"newPwd"}`, http.StatusUnauthorized, `Unauthorized`, userToken},
+		// Test as admin, changing password
+		{"ec4e2897-4ca4-4694-94d7-96db81ec223f", `{"password":"newPwd"}`, http.StatusOK, `"password":"newPwd"`, adminToken},
+		// Test as admin, promoting another user to admin
+		{"39c71853-6206-4eef-9f5b-7a1a90830ccc", `{"isAdmin":true}`, http.StatusOK, `"isAdmin":true`, adminToken},
+	}
+
+	for _, r := range requests {
+		// Create a request to pass to our handler.
+		req, err := http.NewRequest("PUT", "/v1/user/"+r.id, strings.NewReader(r.body))
+
+		if r.token != "" {
+			req.Header.Set("Authorization", "Bearer "+r.token)
+			t.Logf("Request with token : %v", r.token)
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w := mockRequest(req)
+		checkResponse(t, w, r.wantedCode, r.bodyIncl)
+	}
+}
+
